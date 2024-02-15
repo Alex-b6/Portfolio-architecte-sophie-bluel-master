052cd802cf324723e5533d7ece0d6cdbd2bb83f1
AlexB6 <brou.alexandre.pro@gmail.com>
2024-02-15 11:32:15 +0100

    Add: added preview image when we add pics.

--- FrontEnd/assets/script/modale.js
+++ FrontEnd/assets/script/modale.js
@@ -312,6 +312,23 @@ const backToModale = function(e) {
 
 const btnAjouterProjet = document.querySelector(".js-add-work");
 btnAjouterProjet.addEventListener("click", addWork);
+const imageInput = document.querySelector(".js-image");
+const previewImg = document.querySelector(".js-image-preview") ;
+const formgpePhoto = document.querySelector(".form-group-photo") ;
+
+// Ajouter un écouteur d'événements "change" à l'élément d'entrée de fichier
+imageInput.addEventListener("change", function(event) {
+    // Code pour mettre à jour l'aperçu de l'image ici
+    const selectedImage = event.target.files[0];
+
+    if (selectedImage) {
+        const imagePreview = document.querySelector(".js-image-preview");
+        imagePreview.src = URL.createObjectURL(selectedImage);
+        previewImg.style.display="block";
+        beforePreview.style.display="none";
+        formgpePhoto.classList.add("hide-after");
+    }
+});
 
 // Ajouter un projet
 async function addWork(event) {
@@ -319,8 +336,8 @@ async function addWork(event) {
 
     const title = document.querySelector(".js-title").value;
     const categoryId = document.querySelector(".js-categoryId").value;
-    const image = document.querySelector(".js-image").files[0];
-
+    const imageInput = document.querySelector(".js-image");
+    const image = imageInput.files[0];
 
     if (title === "" || categoryId === "" || image === undefined) {
         alert("Merci de remplir tous les champs");
@@ -328,36 +345,43 @@ async function addWork(event) {
     } else if (categoryId !== "1" && categoryId !== "2" && categoryId !== "3") {
         alert("Merci de choisir une catégorie valide");
         return;
-        } else {
-    try {
-        const formData = new FormData();
-        formData.append("title", title);
-        formData.append("category", categoryId);
-        formData.append("image", image);
+    } else {
+        try {
+            const formData = new FormData();
+            formData.append("title", title);
+            formData.append("category", categoryId);
+            formData.append("image", image);
 
-        const response = await fetch("http://localhost:5678/api/works", {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            body: formData,
-        });
+            // Afficher la miniature avant l'ajout
+            const imagePreview = document.querySelector(".js-image-preview");
+            imagePreview.src = URL.createObjectURL(image);
 
-        if (response.status === 201) {
-            alert("Projet ajouté avec succès");
-            modaleProjets(dataAdmin);
-            backToModale(event);
-            generationProjets(data, null);
-            
-        } else if (response.status === 400) {
-            alert("Merci de remplir tous les champs");
-        } else if (response.status === 500) {
-            alert("Erreur serveur");
-        } else if (response.status === 401) {
-            alert("Vous n'êtes pas autorisé à ajouter un projet");
-            window.location.href = "login.html";
-    }}
+            const response = await fetch("http://localhost:5678/api/works", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                body: formData,
+            });
 
-    catch (error) {
-        console.log(error);
-}}}
+            if (response.status === 201) {
+                alert("Projet ajouté avec succès");
+                modaleProjets(dataAdmin);
+                backToModale(event);
+                generationProjets(data, null);
+            } else if (response.status === 400) {
+                alert("Merci de remplir tous les champs");
+            } else if (response.status === 500) {
+                alert("Erreur serveur");
+            } else if (response.status === 401) {
+                alert("Vous n'êtes pas autorisé à ajouter un projet");
+                window.location.href = "login.html";
+            }
+
+            // Réinitialiser l'aperçu de l'image après l'ajout
+            imagePreview.src = "";
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
